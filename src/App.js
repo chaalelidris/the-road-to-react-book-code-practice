22975c4c95b66ac56054f11da8f0968b69f84299
idrisdev <chaalelidris8@gmail.com>
2023-01-08 21:06:35 +0100

    cummon variation

--- src/App.js
+++ src/App.js
@@ -67,22 +67,22 @@ const Search = ({ search, onSearch }) => (
 // List component
 const List = ({ list }) =>
   <ol>
-    {list.map(({ objectID, ...item }) =>
+    {list.map(item =>
       <Item
-        key={objectID}
-        {...item}
+        key={item.objectID}
+        item={item}
       />)}
   </ol>
 
 
-const Item = ({ title, url, author, num_comments, points }) =>
+const Item = ({ item }) =>
   <li >
     <span>
-      <a href={url}>{title} | </a>
+      <a href={item.url}>{item.title} | </a>
     </span>
-    <span>{author} | </span>
-    <span>{num_comments} | </span>
-    <span>{points} | </span>
+    <span>{item.author} | </span>
+    <span>{item.num_comments} | </span>
+    <span>{item.points} | </span>
   </li>
 
 
